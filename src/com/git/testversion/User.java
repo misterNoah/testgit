89498c9bd3f732dc8bb6bac1dcaf2ed18e63216d
123545052@qq.com <123545052@qq.com>
2017-10-11 17:03:13 +0800

    添加age

--- src/com/git/testversion/User.java
+++ src/com/git/testversion/User.java
@@ -3,6 +3,33 @@ package com.git.testversion;
 public class User {
 	private int id;
 	
-	private String name;
+	private String age;
+	
+	private String sex;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	
 }
