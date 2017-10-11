d9b161ae710af48b50e72c2b65712135627962e7
Noah <123545052@qq.com>
2017-10-11 22:16:45 +0800

    111

--- src/com/git/testversion/User.java
+++ src/com/git/testversion/User.java
@@ -12,6 +12,18 @@ public class User {
 	
 	private int id2;
 	
+	private int id3;
+	
+	public int getId2() {
+		return id2;
+	}
+
+	public void setId2(int id2) {
+		this.id2 = id2;
+	}
+
+
+	
 	private Long IDCard;
 	
 	public Long getIDCard() {
