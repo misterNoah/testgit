f66e65453678e1db2b4689b4a7beded31e0a2304
misterNoah <30922797+misterNoah@users.noreply.github.com>
2017-10-11 17:18:14 +0800

    第二次修改Test类
    增加name属性

--- src/com/git/testversion/GitVersionTest.java
+++ src/com/git/testversion/GitVersionTest.java
@@ -3,6 +3,8 @@ package com.git.testversion;
 public class GitVersionTest {
 	
 	private int age;
+	
+	private String name;
 
 	public static void main(String[] args) {
 		System.out.println("hello word");
