fb004ba1f5d46b93f43823b85bb3c4e435a88047
Noah <123545052@qq.com>
2017-10-11 19:49:44 +0800

    冲突提交

--- src/com/git/testversion/GitVersionTest.java
+++ src/com/git/testversion/GitVersionTest.java
@@ -2,7 +2,9 @@ package com.git.testversion;
 
 public class GitVersionTest {
 	
+	private int age;
 	
+	private String name;
 
 	public static void main(String[] args) {
 		System.out.println("hello word");
--- src/com/git/testversion/User.java
+++ src/com/git/testversion/User.java
@@ -3,8 +3,54 @@ package com.git.testversion;
 public class User {
 	private int id;
 	
+	private String age;
+	
+	private String sex;
+	
 	private String name;
 	
 	private String nickName;
+	
+	public User(){
+		
+	}
+	
+	public User(String name, String age){
+		this.name = name;
+		this.age = age;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
